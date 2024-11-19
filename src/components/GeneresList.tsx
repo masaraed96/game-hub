8792b98fd230985d67d7ddde6d9a095767f0abf5
masa abdalhalim <masaabdalhalim@masas-MacBook-Pro.local>
2024-11-19 20:28:31 +0100

    adding generes images

--- src/components/GeneresList.tsx
+++ src/components/GeneresList.tsx
@@ -5,22 +5,20 @@ import getOptimizedImage from "../services/image-url";
 const GeneresList = () => {
   const { data, error, isLoading } = useGeneres();
   return (
-    <ul>
+    <List>
       {data.map((genere: Genre) => (
-        <List>
-          <ListItem key={genere.id} paddingY="5px">
-            <HStack>
-              <Image
-                src={getOptimizedImage(genere.image_background)}
-                boxSize="32px"
-                borderRadius="5px"
-              />
-              <Text fontSize="lg">{genere.name}</Text>
-            </HStack>
-          </ListItem>
-        </List>
+        <ListItem key={genere.id} paddingY="5px">
+          <HStack>
+            <Image
+              src={getOptimizedImage(genere.image_background)}
+              boxSize="32px"
+              borderRadius="5px"
+            />
+            <Text fontSize="lg">{genere.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
