1140221a46797ccffbc7f511b50bc82a08eb6f28
masa abdalhalim <masaabdalhalim@masas-MacBook-Pro.local>
2024-11-19 10:20:04 +0100

    custom fetch games hook

--- src/components/GamesList.tsx
+++ src/components/GamesList.tsx
@@ -1,26 +1,8 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
 
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchedGamesResponse {
-  results: Game[];
-  count: number;
-}
 const GamesList = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchedGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
