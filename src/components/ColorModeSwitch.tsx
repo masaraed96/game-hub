ff031698f75340d3ec6e40ae1f5408c0fcd29c0b
masa abdalhalim <masaabdalhalim@masas-MacBook-Pro.local>
2024-11-19 09:25:18 +0100

    adding color switch

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,20 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+
+const ColorModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <HStack>
+      <Switch
+        colorScheme="green"
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      />
+
+      <Text padding="10px">
+        {colorMode === "dark" ? "to Light mode" : "to Dark mode"}
+      </Text>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;
