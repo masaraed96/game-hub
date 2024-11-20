319ff4156d14b8a4d7ad787bc9fd3e546bdfbb4c
masa abdalhalim <masaabdalhalim@masas-MacBook-Pro.local>
2024-11-20 10:28:41 +0100

    adding placeholder image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import imagePlaceholder from "../assets/no-image-placeholder.webp";
+
 const getOptimizedImage = (url: string)=>{
-    if (!url) return ""
+    if (!url) return imagePlaceholder;
     const target = "media/";
     const index = url.indexOf(target) + target.length;
     return url.slice(0, index) + "crop/" + "600/400/" + url.slice(index);
